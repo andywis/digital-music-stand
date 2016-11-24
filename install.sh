b178851c02d7afe70386092ba88226219aa5abfd
Andy Watkins <andy@wis.co.uk>
2016-11-24 18:23:37 +0000

    use dot instead of source so it works on raspbian

--- install.sh
+++ install.sh
@@ -21,24 +21,25 @@ then
 
 	if [ -z  `which virtualenv` ]
     then
-		echo "INSTALL VIRTUALENV AND THEN COME BACK"
+		echo "Please install virtualenv and then try again."
+        echo "(Something like \"sudo pip install virtualenv\")"
 	echo "Exiting"
 		exit 1
 	fi
 
-    echo "The virtual env is not set up. Will do that now"
+    echo "Setting up the Virtual environment"
     cp pip_requirements.txt $TARGET/pip_requirements.txt
 	where_was_i=`pwd`
     cd $TARGET
 	virtualenv venv
-    source venv/bin/activate
+    . $TARGET/venv/bin/activate
     pip install -r $TARGET/pip_requirements.txt
     rm $TARGET/pip_requirements.txt
     cd $where_was_i
-    echo "The virtual env should not be set up"
+    echo "The virtual env should now be set up"
 
 else
-    source $TARGET/venv/bin/activate
+    . $TARGET/venv/bin/activate
 fi
 
 mkdir -p $TARGET/dms  # where the flask app is stored
